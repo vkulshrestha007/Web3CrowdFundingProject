71f131415cc0521d7c1e68cd46efaa2148abc405
Deep Kulshrestha <Deep.Kulshrestha@coindcx.com>
2023-04-09 19:27:48 +0530

    review comments update

--- hardhat-tutorial/contracts/Batch4Team1Coin.sol
+++ hardhat-tutorial/contracts/Batch4Team1Coin.sol
@@ -4,17 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Batch4Team1Coin is ERC20 {
-
     // address private _owner;
-    
-    constructor(/*uint256 initialSupply*/) ERC20("Batch4Team1Coin", "B4T1") {
-        
-        // _owner = msg.sender;
-        // _mint(msg.sender, initialSupply * 10 ** 18);
-    }
+
+    constructor() ERC20("Batch4Team1Coin", "B4T1") {}
 
     function mint() external {
         _mint(msg.sender, 10 * 10 ** 18);
     }
-
 }
